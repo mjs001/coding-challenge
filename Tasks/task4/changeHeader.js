0a0957c9fb9f12348cb37ed371e46d775061cb1f
mjs001 <50973969+mjs001@users.noreply.github.com>
2024-11-20 11:07:19 -0600

    updated css and organized code better

--- Tasks/task4/changeHeader.js
+++ Tasks/task4/changeHeader.js
@@ -1,8 +1,12 @@
 let clicked = false;
+const header = document.getElementById("editable-header");
+const button = document.getElementById("edit-btn");
 
-const changeHeader = () => {
+button.addEventListener("click", function(e) {
   clicked = !clicked;
-  const header = document.getElementById("editable-header");
   const headerText = clicked ? "Although, I prefer to go by Myco." : "Hello, my name is Miriam.";
+  header.classList.remove("focus");
+  void header.offsetWidth;
   header.textContent = headerText;
-}
+  header.classList.add("focus");
+})
