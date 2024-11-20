61e3cc7f560c58e896ca35d8388fbf426bf22fc4
mjs001 <50973969+mjs001@users.noreply.github.com>
2024-11-20 01:45:48 -0600

    added in style changes

--- Tasks/task4/changeHeader.js
+++ Tasks/task4/changeHeader.js
@@ -5,5 +5,5 @@ const changeHeader = () => {
   clicked = !clicked;
   let text = "";
   clicked ? text = " Although, I prefer to go by Myco." : text = "Hello, my name is Miriam.";
-  header.innerHTML = text;
+  header.textContent = text;
 }
